3f0089fcc3916ba3613dd872cb34c63ca7ff9073
torchhound <jcieslik@umail.iu.edu>
2018-06-04 19:06:19 -0400

    Cleaned up testing, working on output, reduce output is almost 4 times too long

--- lib/output_writer.ex
+++ lib/output_writer.ex
@@ -4,12 +4,6 @@ defmodule OutputWriter do
   end
 
   defp stream do
-    IO.stream(:stdio, :line) |> write_output
-  end
-
-  defp write_output(stream) do
-  	data = Enum.to_list(stream)
-  	:ok = File.write(Path.join("test", "output.txt"), data)
-  	#Enum.each(stream, fn line -> File.write(Path.join("test", "output.txt"), line) end)
+    Enum.each(IO.stream(:stdio, :line), fn line -> File.write(Path.join("test", "output.txt"), line) end)
   end
 end
